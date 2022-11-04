c0088a0874344708c6a6581f11248943095b0735
Moha Boukhatem <moha94orly@gmail.com>
2022-11-04 11:34:18 +0100

    changes

--- app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('contact',views.contact,name='contact'),
-    path('',views.index,name='index'),
-    path('portfolio',views.portfolio,name='portfolio'),
-    path('resume',views.resume,name='resume')
-]
